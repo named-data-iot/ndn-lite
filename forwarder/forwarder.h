50ff73c1c9aaa0b665ae8307d66d418b44d884ce
Zhiyi Zhang <zhiyi@cs.ucla.edu>
2018-10-22 13:47:27 -0700

    init forwarder files

---
+++ forwarder/forwarder.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2018 Zhiyi Zhang
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+#ifndef FORWARDER_FOWARDER_H_
+#define FORWARDER_FOWARDER_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+  typedef struct ndn_forwarder {
+    uint8_t holder;
+  } ndn_forwarder_t;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // FORWARDER_FOWARDER_H_
