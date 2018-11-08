1f268cefc1256ca43afa14bbe376723d9ccc1aa5
Zhiyi Zhang <zhiyi@cs.ucla.edu>
2018-11-08 15:30:06 -0800

    add init code for face receive

--- forwarder/face.c
+++ forwarder/face.c
@@ -1,16 +1,33 @@
-//
-//  face.c
-//  riot-forwarder
-//
-//  Created by Xinyu Ma on 10/31/18.
-//  Copyright © 2018 UCLA. All rights reserved.
-//
+/*
+ * Copyright (C) 2018 Xinyu Ma, Zhiyi Zhang
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
 
 #include "face.h"
 
 int
 ndn_face_receive(ndn_face_t* self, const uint8_t* packet, uint32_t size)
 {
-  // After receiving a packet....
+  (void)self;
+
+  ndn_decoder_t decoder;
+  decoder_init(&decoder, &packet[9], size);
+  uint32_t probe = 0;
+  decoder_get_type(&decoder, &probe);
+  if (probe == TLV_Data) {
+    decoder_get_length(&decoder, &probe);
+    ndn_name_t data_name;
+    ndn_name_tlv_decode(&decoder, &data_name);
+    // check against PIT and trigger callbacks
+  }
+  else if (probe == TLV_Interest) {
+    // TBD
+  }
+  else {
+    // ignore
+  }
   return 0;
 }
--- forwarder/face.h
+++ forwarder/face.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 Zhiyi Zhang
+ * Copyright (C) 2018 Zhiyi Zhang, Xinyu Ma
  *
  * This file is subject to the terms and conditions of the GNU Lesser
  * General Public License v2.1. See the file LICENSE in the top level
@@ -16,18 +16,6 @@
 extern "C" {
 #endif
 
-enum {
-  NDN_FACE_UP, // available face
-  NDN_FACE_DOWN, // temporally down
-  NDN_FACE_CLOSED, // closed face
-  NDN_FACE_FAILED, // closed caused by failure(s)
-};
-
-enum {
-  NDN_FACE_LOCAL, // connects to local process
-  NDN_FACE_REMOTE, // connects to network
-};
-    
 struct ndn_face;
 typedef int(*ndn_iface_send_func)(struct ndn_face* self, const uint8_t* packet, uint32_t size);
 typedef int(*ndn_iface_close_func)(struct ndn_face* self);
@@ -38,11 +26,11 @@ typedef struct ndn_iface{
   ndn_iface_close_func close;
   ndn_iface_destructor destroy;
 } ndn_iface_t;
-    
+
 typedef struct ndn_face {
   const ndn_iface_t* klass;  // sign of derived class
   void* extension;    // Derived class members (we don't have malloc!)
-  
+
   uint16_t face_id;
   uint8_t state;
   uint8_t type;
@@ -52,25 +40,27 @@ typedef struct ndn_face {
 } ndn_face_t;
 
 typedef ndn_face_t ndn_face_table_t[NDN_FACE_TABLE_MAX_SIZE];
-    
+
 static inline int
 ndn_face_send(ndn_face_t* self, const uint8_t* packet, uint32_t size)
 {
-    return self->klass->send(self, packet, size);
+  return self->klass->send(self, packet, size);
 }
-    
+
 static inline int
 ndn_face_close(ndn_face_t* self)
 {
-    return self->klass->close(self);
+  return self->klass->close(self);
 }
 
 static inline void
 ndn_face_destroy(ndn_face_t* self)
 {
-    self->klass->destroy(self);
+  self->klass->destroy(self);
 }
 
+// The function calls the on_data callbacks and on_interest callbacks
+// supposed to be invoked by the adaptation layer functions
 int
 ndn_face_receive(ndn_face_t* self, const uint8_t* packet, uint32_t size);
 
