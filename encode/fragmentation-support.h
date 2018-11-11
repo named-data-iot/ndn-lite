8eb848d1736611608a65999484f023fc17abda5d
Zhiyi Zhang <zhiyi@cs.ucla.edu>
2018-11-10 17:12:10 -0800

    finish frag support

--- encode/fragmentation-support.h
+++ encode/fragmentation-support.h
@@ -6,8 +6,8 @@
  * directory for more details.
  */
 
-#ifndef NDN_ENCODING_FRAGMENTATION_H
-#define NDN_ENCODING_FRAGMENTATION_H
+#ifndef NDN_ENCODING_FRAGMENTATION_SUPPORT_H
+#define NDN_ENCODING_FRAGMENTATION_SUPPORT_H
 
 /*
  * Reuse the ndn-riot fragmentation header (3 bytes header)
@@ -25,6 +25,14 @@
  *    9th to 24th bit: identification (2-byte random number)
  */
 
+#include "ndn-constants.h"
+#include <inttypes.h>
+#include <string.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #define NDN_FRAG_HDR_LEN 3 // Size of the NDN L2 fragmentation header
 #define NDN_FRAG_HB_MASK 0x80 // 1000 0000
 #define NDN_FRAG_MF_MASK 0x20 // 0010 0000
@@ -35,11 +43,11 @@ typedef struct ndn_fragmenter {
   const uint8_t* original;
   uint32_t original_size;
   uint32_t fragment_max_size;
-  uint8_t total_seq;
   uint16_t frag_identifier;
 
+  uint32_t total_frag_num; // total frags to be generated
   uint32_t offset;
-  uint8_t seq; // NEXT seq to be used
+  uint32_t counter; // how many frags have been generated
 } ndn_fragmenter_t;
 
 static inline void
@@ -49,18 +57,20 @@ ndn_fragmenter_init(ndn_fragmenter_t* fragmenter, const uint8_t* original, uint3
   fragmenter->original = original;
   fragmenter->original_size = original_size;
   fragmenter->fragment_max_size = fragment_max_size;
-  fragmenter->total_seq = fragmenter->original_size / (fragment_max_size - 3);
-  fragmenter->total_seq += 1;
   fragmenter->frag_identifier = frag_identifier;
 
+  fragmenter->total_frag_num = fragmenter->original_size / (fragment_max_size - 3);
+  int rem = fragmenter->original_size % (fragment_max_size - 3);
+  if (rem != 0)
+    fragmenter->total_frag_num += 1;
   fragmenter->offset = 0;
-  fragmenter->seq = 0;
+  fragmenter->counter = 0;
 }
 
 static inline uint32_t
 ndn_fragmenter_probe_required_size(ndn_fragmenter_t* fragmenter)
 {
-  return fragmenter->total_seq*3 + fragmenter->original_size;
+  return fragmenter->total_frag_num * 3 + fragmenter->original_size;
 }
 
 // generate ONE fragmented packet
@@ -68,26 +78,29 @@ ndn_fragmenter_probe_required_size(ndn_fragmenter_t* fragmenter)
 static inline int
 ndn_fragmenter_fragment(ndn_fragmenter_t* fragmenter, uint8_t* fragmented)
 {
-  if (fragmenter->seq > fragmenter->total_seq)
-    return NDN_ERROR_OVERSIZE;
+  if (fragmenter->counter == fragmenter->total_frag_num)
+    return NDN_ERROR_FRAG_NO_MORE_FRAGS;
+
+  memset(fragmented, 0, fragmenter->fragment_max_size);
+
+  uint8_t is_last = (fragmenter->counter == fragmenter->total_frag_num - 1)? 1:0;
+  uint8_t seq = fragmenter->counter % (NDN_FRAG_MAX_SEQ_NUM + 1);
 
   // header
-  fragmented[0] = fragmenter->seq;
-  if (fragmenter->seq < fragmenter->total_seq)
+  fragmented[0] = seq;
+  if (is_last)
     fragmented[0] |= NDN_FRAG_MF_MASK;
   fragmented[0] |= NDN_FRAG_HB_MASK;
   fragmented[1] = (fragmenter->frag_identifier >> 8) & 0xFF;
   fragmented[2] = fragmenter->frag_identifier & 0xFF;
 
   // payload
-  int size = (fragmenter->seq == fragmenter->total_seq)?
+  int size = (is_last)?
     fragmenter->original_size - fragmenter->offset: fragmenter->fragment_max_size - 3;
-  memcpy(fragmented + 3, &fragmenter->original[offset], size);
+  memcpy(&fragmented[3], &fragmenter->original[fragmenter->offset], size);
 
   // update state
-  fragmenter->seq++;
-  if (fragmenter->seq == NDN_FRAG_MAX_SEQ_NUM + 1)
-    fragmenter == 0;
+  fragmenter->counter++;
   fragmenter->offset += size;
   return 0;
 }
@@ -116,7 +129,6 @@ ndn_frag_assembler_init(ndn_frag_assembler_t* assembler, uint8_t* original, uint
 static inline int
 ndn_frag_assembler_assemble_frag(ndn_frag_assembler_t* assembler, uint8_t* frag, uint32_t fra_size)
 {
-
   int first_time = (assembler->offset == 0)? 1 : 0;
   // get seq
   uint8_t seq = frag[0] & NDN_FRAG_SEQ_MASK;
@@ -127,13 +139,17 @@ ndn_frag_assembler_assemble_frag(ndn_frag_assembler_t* assembler, uint8_t* frag,
     assembler->seq = 0;
 
   // get identifier
-  uint16_t id = (uint16_t)frag[1] << 8 + (uint16_t)frag[2];
+  uint16_t id = ((uint16_t)frag[1] << 8) + (uint16_t)frag[2];
   if (assembler->frag_identifier != id && first_time == 0)
     return NDN_ERROR_FRAG_WRONG_IDENTIFIER;
   if (first_time)
     assembler->frag_identifier = id;
 
-  memcpy(assembler->original[assembler->offset], &frag[3], fra_size - 3);
+  if (assembler->original_max_size < assembler->offset + fra_size - 3)
+    return NDN_ERROR_OVERSIZE;
+
+  memcpy(&assembler->original[assembler->offset], &frag[3], fra_size - 3);
+  assembler->offset += fra_size - 3;
 
   // get MF bit
   uint8_t mf = frag[0] & NDN_FRAG_MF_MASK;
@@ -144,4 +160,8 @@ ndn_frag_assembler_assemble_frag(ndn_frag_assembler_t* assembler, uint8_t* frag,
   return 0;
 }
 
-#endif // NDN_ENCODING_FRAGMENTATION_H
+#ifdef __cplusplus
+}
+#endif
+
+#endif // NDN_ENCODING_FRAGMENTATION_SUPPORT_H
