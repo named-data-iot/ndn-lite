e50355433b91d333c7538f422b80ac2c19b1d1e4
Zhiyi Zhang <zhiyi@cs.ucla.edu>
2018-12-31 16:48:40 -0800

    forwarder.h: add comments

--- forwarder/forwarder.h
+++ forwarder/forwarder.h
@@ -17,34 +17,76 @@
 extern "C" {
 #endif
 
+/**
+ * The structure to present NDN-Lite forwarder.
+ * We will support content support in future versions.
+ * The NDN forwarder is a singleton in an application.
+ */
 typedef struct ndn_forwarder {
+  /**
+   * The forwarding information base (FIB).
+   */
   ndn_fib_t fib;
+  /**
+   * The pending Interest table (PIT).
+   */
   ndn_pit_t pit;
 } ndn_forwarder_t;
 
-// Get a running instance of current system
+/**
+ * Get a running instance of forwarder.
+ * @return the pointer to the forwarder instance.
+ */
 ndn_forwarder_t*
 ndn_forwarder_get_instance(void);
 
+/**
+ * Init the NDN-Lite forwarder.
+ * This function should be invoked before any face registration and packet sending.
+ * @return the pointer to the forwarder instance.
+ */
 ndn_forwarder_t*
 ndn_forwarder_init(void);
 
+/**
+ * Add FIB entry into the FIB.
+ * This function should be invoked before sending a packet through the specific face.
+ * @param name_prefix. Input. The FIB's name prefix.
+ * @param face. Input/Output. The face instance to send the packet out.
+ * @param cost. The cost of sending a packet through the @param face. When more than one faces
+ *        can be used to send a packet, the face with lower cost will be used.
+ * @return 0 if there is no error.
+ */
 int
 ndn_forwarder_fib_insert(const ndn_name_t* name_prefix,
                          ndn_face_intf_t* face, uint8_t cost);
 
-// Recv data packet
-// name [optional] Decoded data name if it's ready,
-//                 then forwarder won't decode name again
-// Supposed to be invoked by face ONLY
+/**
+ * Let the forwarder receive a Data packet.
+ * This function is supposed to be invoked by face implementation ONLY.
+ * @param self. Input/Output. The forwarder to receive the Data packet.
+ * @param face. Input. The face instance who transmits the packet to the forwarder.
+ * @param name. [optional] Input. The name of the packet. If name == NULL, the forwarder
+ *        will decode the packet name by itself.
+ * @param raw_data. Input. The wire format Data received by the @param face.
+ * @param size. Input. The size of the wire format Data.
+ * @return 0 if there is no error.
+ */
 int
 ndn_forwarder_on_incoming_data(ndn_forwarder_t* self, ndn_face_intf_t* face, ndn_name_t *name,
                                const uint8_t *raw_data, uint32_t size);
 
-// Recv interest packet
-// name [optional] Decoded interest name if it's ready,
-//                 then forwarder won't decode name again
-// Supposed to be invoked by face ONLY
+/**
+ * Let the forwarder receive a Interest packet.
+ * This function is supposed to be invoked by face implementation ONLY.
+ * @param self. Input/Output. The forwarder to receive the Interest packet.
+ * @param face. Input. The face instance who transmits the packet to the forwarder.
+ * @param name. [optional] Input. The name of the packet. If name == NULL, the forwarder
+ *        will decode the packet name by itself.
+ * @param raw_data. Input. The wire format Interest received by the @param face.
+ * @param size. Input. The size of the wire format Interest.
+ * @return 0 if there is no error.
+ */
 int
 ndn_forwarder_on_incoming_interest(ndn_forwarder_t* self, ndn_face_intf_t* face, ndn_name_t *name,
                                    const uint8_t *raw_interest, uint32_t size);
