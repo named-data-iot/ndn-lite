82239034127ca7febd34523bdaf81864a0e2fa8e
Tianyuan  Yu <royu9710@outlook.com>
2019-01-01 23:28:03 +0800

    metainfo.h: add comments

--- encode/metainfo.h
+++ encode/metainfo.h
@@ -7,9 +7,21 @@
 extern "C" {
 #endif
 
+/**
+ * The structure to represent the Metainfo structure.
+ */
 typedef struct ndn_metainfo {
+  /**
+   * The content type the Data packet holds.
+   */
   uint8_t content_type;
+  /**
+   * The freshness period of the Data packet.
+   */
   uint8_t freshness_period[4];
+  /**
+   * The last name component in Name.
+   */
   name_component_t final_block_id;
 
   uint8_t enable_ContentType;
@@ -17,6 +29,10 @@ typedef struct ndn_metainfo {
   uint8_t enable_FinalBlockId;
 } ndn_metainfo_t;
 
+/**
+ * Init an Metainfo structure.
+ * @param meta. Output. The Metainfo structure to be inited.
+ */
 static inline void
 ndn_metainfo_init(ndn_metainfo_t* meta)
 {
@@ -25,20 +41,41 @@ ndn_metainfo_init(ndn_metainfo_t* meta)
   meta->enable_FinalBlockId = 0;
 }
 
-// will do memory copy
+/**
+ * Init an Metainfo structure. This function will fo memory copy.
+ * @param meta. Output. The Metainfo structure to be inited.
+ * @param other. Output. The Metainfo structure to be copied from.
+ */
 static inline void
 ndn_metainfo_from_other(ndn_metainfo_t* meta, const ndn_metainfo_t* other)
 {
   memcpy(meta, other, sizeof(ndn_metainfo_t));
 }
 
+/**
+ * Decode the Metainfo from wire format (TLV block).
+ * @param decoder. Input. The decoder who keeps the decoding result and the state.
+ * @param meta. Output. The Metainfo decoded from TLV block.
+ * @return 0 if there is no error.
+ */
 int
 ndn_metainfo_tlv_decode(ndn_decoder_t* decoder, ndn_metainfo_t* meta);
 
-// add a _tlv_ to avoid conflicts with the existing encoding
+/**
+ * Decode an Metainfo TLV block into an Metainfo.
+ * @param meta. Output. The Metainfo to which the TLV block will be decoded.
+ * @param block_value. Input. The Metainfo TLV block buffer.
+ * @param block_size. Input. The size of the Metainfo TLV block buffer.
+ * @return 0 if decoding is successful.
+ */
 int
 ndn_metainfo_from_tlv_block(ndn_metainfo_t* meta, const uint8_t* block_value, uint32_t block_size);
 
+/**
+ * Set ContentType of the Metainfo.
+ * @param meta. Output. The Metainfo whose ContentType will be set.
+ * @param content_type. Input. ContentType value following NDN Packet Format Specification 0.3.
+ */
 static inline void
 ndn_metainfo_set_content_type(ndn_metainfo_t* meta, uint8_t content_type)
 {
@@ -46,8 +83,13 @@ ndn_metainfo_set_content_type(ndn_metainfo_t* meta, uint8_t content_type)
   meta->content_type = content_type;
 }
 
+/**
+ * Set FreshnessPeriod of the Metainfo.
+ * @param meta. Output. The Metainfo whose FreshnessPeriod will be set.
+ * @param freshness_period. Input. FreshnessPeriod value.
+ */
 static inline void
-ndn_metainfo_set_freshness_period(ndn_metainfo_t *meta, uint32_t freshness_period)
+ndn_metainfo_set_freshness_period(ndn_metainfo_t* meta, uint32_t freshness_period)
 {
   meta->enable_FreshnessPeriod = 1;
   meta->freshness_period[0] = (freshness_period >> 24) & 0xFF;
@@ -56,6 +98,11 @@ ndn_metainfo_set_freshness_period(ndn_metainfo_t *meta, uint32_t freshness_perio
   meta->freshness_period[3] = freshness_period & 0xFF;
 }
 
+/**
+ * Set FinalBlockId of the Metainfo.
+ * @param meta. Output. The Metainfo whose FinalBlockId will be set.
+ * @param final_block_id. Input. The last component of Name.
+ */
 static inline void
 ndn_metainfo_set_final_block_id(ndn_metainfo_t *meta, const name_component_t* final_block_id)
 {
@@ -63,6 +110,12 @@ ndn_metainfo_set_final_block_id(ndn_metainfo_t *meta, const name_component_t* fi
   memcpy(&meta->final_block_id, final_block_id, sizeof(name_component_t));
 }
 
+/**
+ * Probe the size of a Metainfo TLV block before encoding it from a Metainfo structure.
+ * This function is used to check whether the output buffer size is enough or not.
+ * @param meta. Input. The Metainfo structure to probe.
+ * @return the length of the Metainfo TLV block.
+ */
 static inline uint32_t
 ndn_metainfo_probe_block_size(const ndn_metainfo_t* meta)
 {
@@ -86,8 +139,14 @@ ndn_metainfo_probe_block_size(const ndn_metainfo_t* meta)
   }
 }
 
+/**
+ * Decode the Metainfo structure into wire format (TLV block).
+ * @param encoder. Output. The encoder who keeps the encoding result and the state.
+ * @param interest. Input. The Metainfo structure to be encoded.
+ * @return 0 if there is no error.
+ */
 int
-ndn_metainfo_tlv_encode(ndn_encoder_t* encoder, const ndn_metainfo_t *meta);
+ndn_metainfo_tlv_encode(ndn_encoder_t* encoder, const ndn_metainfo_t* meta);
 
 #ifdef __cplusplus
 }
