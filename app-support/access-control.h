b5705e4eb1bd5f266098318e28cce233beb37c88
Tianyuan  Yu <royu9710@outlook.com>
2019-01-04 00:18:19 +0800

    access-control.h and service-discovery.h: add initial comments

--- app-support/access-control.h
+++ app-support/access-control.h
@@ -14,28 +14,72 @@
 #include "../encode/interest.h"
 #include "../encode/data.h"
 
+/**
+ * The structure to present an unfinished key only used in 
+ * Access Control EK/DK process.
+ * Library users should not declare this structure in application.
+ */
 typedef struct ndn_ac_unfinished_key {
+  /**
+   * The key id of unfinished key.
+   */
   uint32_t key_id;
+  /**
+   * The locally generated ECC public key.
+   */
   ndn_ecc_pub_t dh_pub;
+  /**
+   * The locally generated ECC private key.
+   */
   ndn_ecc_prv_t dh_prv;
 } ndn_ac_unfinished_key_t;
 
+/**
+ * The structure to implement state storage and management in Access Control.
+ */
 typedef struct ndn_ac_state {
+  /**
+   * The identity of local state manager.
+   */  
   ndn_name_t self_identity;
-
-  // self identity key
+  /**
+   * The identity ECC public key.
+   */
   ndn_ecc_pub_t self_pub_key;
+  /**
+   * The identity ECC private key.
+   */
   ndn_ecc_prv_t self_prv_key;
-
-  // aes key ids
+  /**
+   * The AES key id list in Encryption Request.
+   */
   uint32_t eks[NDN_APPSUPPORT_AC_KEY_LIST_SIZE];
+  /**
+   * The AES key id list in Decryption Request.
+   */
   uint32_t dks[NDN_APPSUPPORT_AC_KEY_LIST_SIZE];
 } ndn_ac_state_t;
 
+/**
+ * Init a Access Control State structure.
+ * @param self_identity. Input. Local state manager identity.
+ * @param self_pub_key. Input. The identity ECC public key.
+ * @param self_prv_key. Input. The identity ECC private key.
+ */
 void
 ndn_ac_state_init(const ndn_name_t* self_identity, const ndn_ecc_pub_t* self_pub_key,
                   const ndn_ecc_prv_t* self_prv_key);
 
+/**
+ * Prepare a Key Request to send. This function will automatically sign and encode the interest.
+ * @param encoder. Output. The encoder to keep the encoded Key Request.
+ *        The encoder should be inited to proper output buffer.
+ * @param home_prefix. Input. The network home prefix to configure the state manager.
+ * @param self_identity. Input. The local state manager identity.
+ * @param prv_key. Input. The ECC private key used to sign the interest.
+ * @param is_ek. Input. Determine whether to encode a Encryption Request or Decryption Request.
+ * @return 0 if there is no error.
+ */
 int
 ndn_ac_prepare_key_request_interest(ndn_encoder_t* encoder,
                                     const ndn_name_t* home_prefix,
@@ -43,17 +87,41 @@ ndn_ac_prepare_key_request_interest(ndn_encoder_t* encoder,
                                     uint32_t ac_key_id, const ndn_ecc_prv_t* prv_key,
                                     uint8_t is_ek);
 
+/**
+ * Process Encryption Request's Response. This function will automatically set and 
+ * update Access Control State.
+ * @param data. Input. Decoded and signature verified Encryption Request's Response Packet.
+ * @return 0 if there is no error.
+ */
 int
 ndn_ac_on_ek_response_process(const ndn_data_t* data);
 
+/**
+ * Process Deryption Request's Response. This function will automatically set and 
+ * update Access Control State.
+ * @param data. Input. Decoded and signature verified Decryption Request's Response Packet.
+ * @return 0 if there is no error.
+ */
 int
 ndn_ac_on_dk_response_process(const ndn_data_t* data);
 
+/*************************/
+/*  APIs for Controller  */
+/*************************/
 
-//controller part
+/**
+ * Process Access Control Request. This function will automatically set and 
+ * update Access Control State on the access controller side.
+ * @param response. Output. Prepared Response.
+ * @param interest. Input. Decoded and signature verified signed interest.
+ * @return 0 if there is no error.
+ */
 int
 ndn_ac_on_interest_process(ndn_data_t* response, const ndn_interest_t* interest);
 
+/*************************************/
+/*  Helper Functions for Controller  */
+/*************************************/
 int
 ndn_ac_prepare_ek_response(ndn_decoder_t* decoder, const ndn_interest_t* interest,
                            ndn_data_t* response);
@@ -62,13 +130,5 @@ int
 ndn_ac_prepare_dk_response(ndn_decoder_t* decoder, const ndn_interest_t* interest,
                            ndn_data_t* response);
 
-/*
-  int
-  ndn_ac_check_policy
-*/
-
-// #ifdef __cplusplus
-// }
-// #endif
 
 #endif // NDN_APP_SUPPORT_ACCESS_CONTROL_H
--- app-support/service-discovery.h
+++ app-support/service-discovery.h
@@ -17,29 +17,79 @@
 extern "C" {
 #endif
 
+/**
+ * The structure to represent a NDN service.
+ */
 typedef struct ndn_service {
+  /**
+   * The NDN service status.
+   */  
   uint8_t status;
+  /**
+   * The NDN service ID.
+   */    
   uint8_t id_value[NDN_APPSUPPORT_SERVICE_ID_SIZE];
+  /**
+   * Size of service ID.
+   */    
   uint32_t id_size;
 } ndn_service_t;
 
+/**
+ * The structure to implement neighbors management in Access Control.
+ */
 typedef struct ndn_sd_identity {
+  /**
+   * The neighbor identity.
+   */ 
   name_component_t identity;
+  /**
+   * The neighbor identity's service list.
+   */ 
   ndn_service_t services[NDN_APPSUPPORT_SERVICES_SIZE];
 } ndn_sd_identity_t;
 
+/**
+ * The structure to implement state storage and management in Service Discovery.
+ */
 typedef struct ndn_sd_context {
+  /**
+   * The home prefix of local network.
+   */ 
   ndn_name_t home_prefix;
+  /**
+   * The local state manager identity (and services provided).
+   */ 
   ndn_sd_identity_t self;
+  /**
+   * The neighbor list.
+   */ 
   ndn_sd_identity_t neighbors[NDN_APPSUPPORT_NEIGHBORS_SIZE];
 } ndn_sd_context_t;
 
+/**
+ * Init a Service Discovery State structure.
+ * @param home_prefix. Input. The network home prefix to configure the state manager.
+ * @param self_id. Input. The local state manager identity.
+ */
 void
 ndn_sd_init(const ndn_name_t* home_prefix, const name_component_t* self_id);
 
+/**
+ * Get a pointer to a NDN service by searching its NDN service ID.
+ * @param id_value. Input. Service ID buffer.
+ * @param id_size. Input. Size of input service ID.
+ * @return pointer to the NDN service if it exists.
+ */
 ndn_service_t*
 ndn_sd_register_get_self_service(const char* id_value, uint32_t id_size);
 
+/**
+ * Set service status of a NDN service.
+ * @param service. Input. The NDN service whose service status will be set.
+ * @param status. Input. Service status value.
+ * @return 0 if there is no error.
+ */
 static inline int
 ndn_sd_set_service_status(ndn_service_t* service, uint8_t status)
 {
@@ -47,32 +97,76 @@ ndn_sd_set_service_status(ndn_service_t* service, uint8_t status)
   return 0;
 }
 
+/**
+ * Find a neighbor in the neighbor list by searching its identity.
+ * @param id. Input. The neighbor identity searched.
+ * @return pointer to the neighbor identity if it exists.
+ */
 ndn_sd_identity_t*
 ndn_sd_find_neigbor(const name_component_t* id);
 
+/**
+ * Find a service provider in the neighbor list by searching service ID.
+ * @param id. Input. The service ID searched.
+ * @param id_size. Input. Size of input service ID.
+ * @return pointer to the first service provider's neighbor identity if it exists.
+ */
 ndn_sd_identity_t*
 ndn_sd_find_first_service_provider(const char* id_value, uint32_t id_size);
 
+/**
+ * Prepare a Service Discovery Advertisement. This function should be called after setting local services status.
+ * @param interest. Output. The prepared advertisement interest.
+ */
 void
 ndn_sd_prepare_advertisement(ndn_interest_t* interest);
 
+/**
+ * Prepare a Service Discovery Query. Users should manually sign the output query 
+ * interest to obtain a valid signed query interest. 
+ * @param interest. Output. The prepared unsigned query interest.
+ * @param target. Input. The query target identity.
+ * @param service. Input. The query target service.
+ * @param params_value. Input. The query parameter buffer (optional)
+ * @param params_size. Input. Size of input buffer (optional)
+ */
 void
 ndn_sd_prepare_query(ndn_interest_t* interest, name_component_t* target, ndn_service_t* service,
                      const uint8_t* params_value, uint32_t params_size);
 
-// function used in receiver side onInterest callback
+/**
+ * Process Service Discovery Advertisement. This function will automatically set and 
+ * update local Service Discovery State and is used in receiver side onInterest callback.
+ * @param interest. Input. Decoded advertisement interest.
+ * @return 0 if there is no error.
+ */
 int
 ndn_sd_on_advertisement_process(const ndn_interest_t* interest);
 
-// function used in receiver side onInterest callback
+/**
+ * Process Service Discovery Query. This function is used in receiver side onInterest callback.
+ * @param interest. Input. Decoded and signature verified query interest.
+ * @param response. Output. Prepared query response.
+ * @return 0 if there is no error.
+ */
 int
 ndn_sd_on_query_process(const ndn_interest_t* interest, ndn_data_t* response);
 
-// function used in sender side onData callback
+/**
+ * Process Service Discovery Query's Response. This function will automatically set and 
+ * update local Service Discovery State and is used in sender side onData callback.
+ * @param response. Input. Decoded and signature verified query response.
+ * @return 0 if there is no error.
+ */
 int
 ndn_sd_on_query_response_process(const ndn_data_t * response);
 
-// function used in sender side onInterestTimeout callback
+/**
+ * Process Service Discovery Query's Timeout. This function will automatically remove query target
+ * identity from local neighbor list and is used in sender side onInterestTimeout callback.
+ * @param interest. Input. Expired interest.
+ * @return 0 if there is no error.
+ */
 int
 ndn_sd_on_query_timeout_process(const ndn_interest_t* interest);
 
