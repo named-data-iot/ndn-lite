513e020a28cc76ac12ee4cdb3bdaf8ebcb13428c
Tianyuan  Yu <royu9710@outlook.com>
2019-01-12 04:39:48 +0800

    fix issue ndn_name_tlv_decode buffer overflow #27

--- encode/metainfo.c
+++ encode/metainfo.c
@@ -1,3 +1,11 @@
+/*
+ * Copyright (C) 2018-2019 Zhiyi Zhang, Tianyuan Yu
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
 #include "metainfo.h"
 
 int
