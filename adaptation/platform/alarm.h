8e38142d0170234bff6ee7880e31b852ee4069fe
RoySCU <royu9710@outlook.com>
2019-02-20 22:31:55 +0800

    timer: add comments for alarm module

--- adaptation/platform/alarm.h
+++ adaptation/platform/alarm.h
@@ -11,19 +11,58 @@
 extern "C" {
 #endif
 
+/**
+ * This method will init alarm.
+ */
+void ndn_platform_alarm_init(void);
+
+/**
+ * This method will de-init alarm.
+ */
+void ndn_platform_alarm_deinit(void);
+
+/**
+ * This method will start alarm.
+ * @param start. Input. Timer start time.
+ * @param delta. Input. Delta between timer start time and expiry time.
+ */
 void ndn_platform_alarm_millis_start(uint32_t start, uint32_t delta);
 
-uint32_t ndn_platform_alarm_millis_get_now(void);
+/**
+ * This method will get current system time (in millisecond) from alarm.
+ * @return Current time
+ */
+uint64_t ndn_platform_alarm_millis_get_now(void);
 
-// reserved, should only be a system call
+/**
+ * This method will get current system time (in microsecond) from alarm.
+ * @return Current time
+ */
+uint64_t ndn_platform_alarm_micros_get_now(void);
+
+/**
+ * This method will stop alarm.
+ * @note reserved, should only be called internally.
+ */
 void ndn_platform_alarm_millis_stop(void);
 
+/**
+ * This method will process alarm.
+ * @param instance. Input. Timer scheduler instance.
+ */
 void ndn_platform_alarm_process(void* instance);
 
-
-// defined in timer.c
+/**
+ * This method will fire millisecond alarm.
+ * @param instance. Input. Timer scheduler instance.
+ */
 extern void ndn_platform_alarm_millis_fire(void* instance);
 
+/**
+ * This method will fire microsecond alarm.
+ * @note NOT SUPPORTED YET.
+ * @param instance. Input. Timer scheduler instance.
+ */
 extern void ndn_platform_alarm_micros_fire(void* instance);
 
 #ifdef __cplusplus
--- adaptation/platform/system.h
+++ adaptation/platform/system.h
@@ -12,16 +12,29 @@
 extern "C" {
 #endif
 
+/**
+ * This method initialize the platform.
+ */
 void ndn_platform_init(void);
 
+/**
+ * This method de-initialize the platform.
+ */
 void ndn_platform_deinit(void);
 
+/**
+ * This method delay the platform in millisecond, like no op.
+ * @param delay. Time to delay in millisecond.
+ */
 void ndn_platform_delay_ms(uint32_t delay);
 
-uint32_t ndn_platform_current_time(void);
+/**
+ * This static method returns the current time in milliseconds.
+ * @return The current time in milliseconds.
+ */
+uint64_t ndn_platform_current_time(void);
 
 
 #ifdef __cplusplus
 } // end of extern "C"
 #endif
-
