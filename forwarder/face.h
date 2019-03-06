cd4755686de57150e1b8706722217bfc9d5cff6f
RoySCU <royu9710@outlook.com>
2019-03-06 22:59:30 +0800

    fix some conflicts

--- forwarder/face.h
+++ forwarder/face.h
@@ -11,9 +11,11 @@
 
 #include "../encode/name.h"
 
+/*
 #define container_of(ptr, type, member) ({                \
   const typeof(((type *)0)->member) *__mptr = (ptr);      \
   (type *)((char *)__mptr - offsetof(type, member)); })
+*/
 
 #ifdef __cplusplus
 extern "C" {
