35342ce8d4a11015edc549b999fb7e1664acafd8
Edward Lu <edwardzlu98@gmail.com>
2019-03-08 12:23:27 -0800

    Merged schematized trust

---
+++ encode/trust-schema/ndn-trust-schema-pattern-component.c
@@ -0,0 +1,116 @@
+
+#include "ndn-trust-schema-pattern-component.h"
+
+#include <string.h>
+#include <stdlib.h>
+
+#include "../../ndn-constants.h"
+#include "../../ndn-error-code.h"
+
+int
+ndn_trust_schema_pattern_component_from_string(ndn_trust_schema_pattern_component_t* component, const char* string, uint32_t size)
+{
+  
+  if (size+1 > NDN_TRUST_SCHEMA_PATTERN_COMPONENT_STRING_MAX_SIZE)
+    return NDN_OVERSIZE;
+  
+  char temp_pattern_comp_string_arr[NDN_TRUST_SCHEMA_PATTERN_COMPONENT_STRING_MAX_SIZE];
+
+  memcpy(temp_pattern_comp_string_arr, string, size);
+  temp_pattern_comp_string_arr[size] = '\0';
+  
+  uint32_t string_size = string[size - 1] == '\0' ? size-1 : size;
+
+  int type = _probe_trust_schema_pattern_component_type(temp_pattern_comp_string_arr);
+
+  if (type == NDN_TRUST_SCHEMA_PATTERN_COMPONENT_UNRECOGNIZED_TYPE)
+    return type;
+
+  switch (type) {
+  case NDN_TRUST_SCHEMA_SINGLE_NAME_COMPONENT:
+    component->type = type;
+    memcpy(component->value, string+1, string_size-2);
+    component->size = string_size-2;
+    break;
+  case NDN_TRUST_SCHEMA_WILDCARD_NAME_COMPONENT:
+    component->type = type;
+    component->size = 0;
+    break;
+  case NDN_TRUST_SCHEMA_SUBPATTERN_INDEX:
+    component->type = type;
+    *component->value = ((int) string[1]) - '0';
+    component->size = NDN_TRUST_SCHEMA_PATTERN_COMPONENT_BUFFER_SIZE;
+    break;
+  case NDN_TRUST_SCHEMA_WILDCARD_NAME_COMPONENT_SEQUENCE:
+    component->type = type;
+    component->size = 0;
+    break;
+  case NDN_TRUST_SCHEMA_WILDCARD_SPECIALIZER:
+    component->type = type;
+    memcpy(component->value, string+1, string_size-2);
+    component->size = string_size-2;
+    break;
+  case NDN_TRUST_SCHEMA_RULE_REF: {
+
+    if (string_size > NDN_TRUST_SCHEMA_RULE_NAME_MAX_LENGTH) {
+      return NDN_TRUST_SCHEMA_RULE_NAME_TOO_LONG;
+    }
+    
+    memcpy(component->value, string, string_size-2);
+    component->value[string_size-2] = '\0';
+    component->type = type;
+    component->size = string_size-2;
+    
+    break;
+  }
+  default:
+    return NDN_TRUST_SCHEMA_PATTERN_COMPONENT_PARSING_ERROR;
+  }
+
+  return 0;
+  
+}
+
+int
+ndn_trust_schema_pattern_component_copy(const ndn_trust_schema_pattern_component_t *lhs, ndn_trust_schema_pattern_component_t *rhs) {
+
+  rhs->type = lhs->type;
+  if (lhs->size > NDN_TRUST_SCHEMA_PATTERN_COMPONENT_BUFFER_SIZE)
+    return NDN_TRUST_SCHEMA_PATTERN_COMPONENT_INVALID_SIZE;
+  memcpy(rhs->value, lhs->value, lhs->size);
+  rhs->subpattern_info = lhs->subpattern_info;
+  rhs->size = lhs->size;
+
+  return 0;
+}
+
+int
+ndn_trust_schema_pattern_component_compare(const ndn_trust_schema_pattern_component_t *pattern_component, const name_component_t *name_component) {
+  
+  // allocate arrays for checking wildcard specializers
+  char temp_wildcard_specializer_string_arr[NDN_TRUST_SCHEMA_PATTERN_COMPONENT_STRING_MAX_SIZE];  
+  char temp_name_component_string_arr[NDN_TRUST_SCHEMA_PATTERN_COMPONENT_STRING_MAX_SIZE];
+  
+  switch (pattern_component->type) {
+  case NDN_TRUST_SCHEMA_SINGLE_NAME_COMPONENT:
+    return (memcmp(pattern_component->value, name_component->value, pattern_component->size) == 0 &&
+	    pattern_component->size == name_component->size) ? 0 : -1;	      
+  case NDN_TRUST_SCHEMA_WILDCARD_SPECIALIZER:
+    memcpy(temp_wildcard_specializer_string_arr, pattern_component->value, pattern_component->size);
+    temp_wildcard_specializer_string_arr[pattern_component->size] = '\0';
+    memcpy(temp_name_component_string_arr, name_component->value, name_component->size);
+    temp_name_component_string_arr[name_component->size] = '\0';
+    int ret_val = re_match(temp_wildcard_specializer_string_arr, temp_name_component_string_arr);
+    return (ret_val != TINY_REGEX_C_FAIL) ? 0 : -1;
+  case NDN_TRUST_SCHEMA_WILDCARD_NAME_COMPONENT:
+  case NDN_TRUST_SCHEMA_WILDCARD_NAME_COMPONENT_SEQUENCE:
+    return 0;
+  case NDN_TRUST_SCHEMA_SUBPATTERN_INDEX:
+  case NDN_TRUST_SCHEMA_RULE_REF:
+    return -1;
+  default:
+    return -1;
+  }
+  return -1;
+
+}
---
+++ encode/trust-schema/ndn-trust-schema-rule.h
@@ -0,0 +1,49 @@
+
+#ifndef NDN_TRUST_SCHEMA_RULE_H
+#define NDN_TRUST_SCHEMA_RULE_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+#include "ndn-trust-schema-pattern.h"
+
+typedef struct ndn_trust_schema_rule {
+  /**
+   * The NDN trust schema pattern which represents the required data name pattern of this rule.
+   */
+  ndn_trust_schema_pattern_t data_pattern;
+
+  /**
+   * The NDN trust schema pattern which represents the required key name pattern of this rule.
+   */
+  ndn_trust_schema_pattern_t key_pattern;
+
+  // will probably eventually put some kind of handler to this rule so that it can be referenced by other
+  // rules or itself
+} ndn_trust_schema_rule_t;
+
+
+/**
+ * Init an NDN Trust Schema rule from two strings. This function will do memory copy and
+ * only support regular string; not support URI currently.
+ * @param rule. Output. The NDN Trust Schema rule to be inited.
+ * @param data_name_pattern_string. Input. The string from which the data name ndn trust schema patter will be inited.
+ * @param data_name_pattern_string_size. Input. Size of the data name pattern string.
+ * @param key_name_pattern_string. Input. The string from which the key name ndn trust schema patter will be inited.
+ * @param key_name_pattern_string_size. Input. Size of the key name pattern string.
+ * @return 0 if there is no error.
+ */
+int
+ndn_trust_schema_rule_from_strings(ndn_trust_schema_rule_t* rule,
+				     const char* data_name_pattern_string, uint32_t data_name_pattern_string_size,
+				     const char* key_name_pattern_string, uint32_t key_name_pattern_string_size);
+/**
+ * Copy the lhs rule to the rhs rule.
+ * @param lhs. Input. The rule to be inited.
+ * @param rhs. Output. The rule that will be copied to.
+ * @return 0 if there is no error.
+ */
+int
+ndn_trust_schema_rule_copy(const ndn_trust_schema_rule_t *lhs, ndn_trust_schema_rule_t *rhs);
+
+#endif // NDN_TRUST_SCHEMA_RULE_H
