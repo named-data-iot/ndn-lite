becfc76d71f99a732d92c3860bf678373751b778
Saurab Dulal <sdulal@memphis.edu>
2019-09-07 18:50:56 -0400

    additional code

--- psync-fconsumer/full-consumer.c
+++ psync-fconsumer/full-consumer.c
@@ -5,28 +5,64 @@ Details: https://redmine.named-data.net/issues/4987
 
 */
 
-#include "../face/dummy-face.h"
+#include "full-consumer.h"
 
-typedef struct
+void 
+init_full_consumer(char* syncPrefix, ndn_udp_face_t* face, 
+                         void (*onUpdate)(void*))
 {
-
-    void (*init_full_consumer);        
-    void (*send_sync_interet);
-    void (*on_sync_data);
-    void (*stop);
+    full_consumer* consumer;
+    consumer->udp_face = face;
     
+    ndn_name_from_string(&consumer->m_syncPrefix, syncPrefix, sizeof(syncPrefix));
+    ndn_name_print(&consumer->m_syncPrefix);
 
-} full_consumer;
+    //  char controller_prefix_string[] = "/ndn/AC";
+    //  ndn_name_t controller_prefix;
+    //  ndn_name_from_string(&controller_prefix, controller_prefix_string, sizeof(controller_prefix_string));
 
-void init_full_consumer()
-{
+
+    // int
+// ndn_name_from_string(ndn_name_t* name, const char* string, uint32_t size);
+    // have to convert char to ndn_name and assign it to m_iblt
+    // consumer->m_iblt = "x%9Cc%60%18%05%A3%60%F8%00%00%02%D0%00%01";
+
+
+
+
+
+
+//   : m_face(face)
+//  , m_scheduler(m_face.getIoService())
+//  , m_syncPrefix(syncPrefix)
+//  , m_iblt("x%9Cc%60%18%05%A3%60%F8%00%00%02%D0%00%01") //empty IBF
+//  , m_onUpdate(onUpdate)
+//  , m_syncInterestLifetime(syncInterestLifetime)
+//  , m_rng(ndn::random::getRandomNumberEngine())
+//  , m_rangeUniformRandom(100, 500)
+// {
+//   int jitter = m_syncInterestLifetime.count() * .20;
+//   m_jitter = std::uniform_int_distribution<>(-jitter, jitter);
+
+//   sendSyncInterest();
+// }
+
 }
 
-
-void send_sync_interet() 
+void 
+send_sync_interet() 
 {
+  
 }
 
-void on_sync_data()
+void
+on_sync_data()
 {
+  
+}
+
+void 
+stop () 
+{
+    // stop running fetcher
 }
---
+++ psync-fconsumer/full-consumer.h
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <ndn-lite.h>
+#include "ndn-lite/encode/name.h"
+#include "ndn-lite/encode/data.h"
+#include "ndn-lite/encode/interest.h"
+#include <time.h>
+#include <stdlib.h>
+
+
+#ifndef FULL_CONSUMER_H
+#define FULL_CONSUMER_H
+
+
+typedef struct 
+{   
+    // Store sequence number for the prefix.
+    ndn_name_t prefix;
+    uint64_t sequence_number;
+} prefixes;
+
+typedef struct
+{
+// define all the required variables i.e. face, io-service and all
+
+    ndn_unix_face_t* unix_face;
+    ndn_udp_face_t* udp_face;
+    ndn_name_t m_syncPrefix;
+    ndn_name_t m_syncInterestPrefix;
+    ndn_name_t m_iblt;
+    ndn_name_t m_syncDataName;
+    uint32_t jitter;
+    // srand(time(NULL)); //we can use this to create a random number
+
+
+} full_consumer;
+
+// const ndn::Name& syncPrefix,
+//                    ndn::Face& face,
+//                    const UpdateCallback& onUpdate,
+//                    ndn::time::milliseconds syncInterestLifetime
+
+
+void init_full_consumer (char* syncPrefix, ndn_udp_face_t* face, 
+                         void (*onUpdate)(void*));
+
+void send_sync_interet();
+
+void on_sync_data(); 
+
+void stop (); 
+
+#endif 
