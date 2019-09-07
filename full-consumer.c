0a03a5f7765f7fe122c86310b779d844e8965bed
Saurab Dulal <sdulal@memphis.edu>
2019-09-07 15:40:22 -0400

    skeleton full consumer

---
+++ full-consumer.c
@@ -0,0 +1,36 @@
+/*
+
+This is a psync full consumer implementation 
+Details: https://redmine.named-data.net/issues/4987
+
+*/
+
+#include "../face/dummy-face.h"
+
+typedef struct
+{
+
+    void (*init_full_consumer);        
+    void (*send_sync_interet);
+    void (*on_sync_data);
+    void (*stop);
+    
+
+} full_consumer;
+
+void init_full_consumer()
+{
+}
+
+
+void send_sync_interet() 
+{
+}
+
+void on_sync_data()
+{
+}
+
+int main (){
+return 0;
+}
