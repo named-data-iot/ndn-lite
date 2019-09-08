f75155342fd54cd1b48813f58f21d7faa3d2cd69
Saurab Dulal <sdulal@memphis.edu>
2019-09-08 09:27:07 -0400

    latest

--- psync-fconsumer/full-consumer.c
+++ psync-fconsumer/full-consumer.c
@@ -22,12 +22,12 @@ init_full_consumer(const char* syncPrefix, ndn_udp_face_t* face,
     
     full_consumer *consumer = (full_consumer*)malloc(sizeof(full_consumer));
     consumer->udp_face = face;
-    error_reporting(ndn_name_from_string(&consumer->m_syncPrefix, syncPrefix, sizeof(syncPrefix)));
+    error_reporting(ndn_name_from_string(&consumer->m_syncPrefix, syncPrefix, strlen(syncPrefix)));
     // ndn_name_print(&consumer->m_syncPrefix);
     
     // construct empty IBLT
-    const char* ibf = "/x%9Cc%60%18%05%A3%60%F8%00%00%02%D0%00%01";
-    error_reporting(ndn_name_from_string(&consumer->m_iblt, ibf, sizeof(ibf)));
+    const char* ibf = "/x\x9C""c""\x60\x18\x05\xA3\x60\xF8\x00\x00\x02\xD0\x00\x01";
+    error_reporting(ndn_name_from_string(&consumer->m_iblt, ibf, strlen(ibf)));
     
     send_sync_interet(consumer);
 
@@ -69,7 +69,7 @@ send_sync_interet(full_consumer* consumer)
 {
   
   ndn_name_append_name(&consumer->m_syncPrefix, &consumer->m_iblt);
-
+  ndn_name_print(&consumer->m_syncPrefix);
   uint8_t buf[4096];
   bool running;
 
