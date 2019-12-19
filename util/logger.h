6575f3b28c7af84fb635c42fb2eae3d065fa9190
RoySCU <royu9710@outlook.com>
2019-12-19 14:17:31 -0800

    util: update logger to support Name

--- util/logger.h
+++ util/logger.h
@@ -11,6 +11,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "../encode/name.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -19,19 +20,27 @@ extern "C" {
 #ifdef ENABLE_NDN_LOG_ERROR
 #define NDN_LOG_ERROR(...) { \
   printf("ERROR: %s, L%d: ", __func__, __LINE__); \
-  printf(__VA_ARGS__);printf("\n"); \
+  printf(__VA_ARGS__); \
+}
+#define NDN_LOG_ERROR_NAME(name) { \
+  ndn_name_print(name);\
 }
 #else
 #define NDN_LOG_ERROR(...)
+#define NDN_LOG_ERROR_NAME(name)
 #endif
 
 #ifdef ENABLE_NDN_LOG_DEBUG
 #define NDN_LOG_DEBUG(...) { \
   printf("DEBUG: %s, L%d: ", __func__, __LINE__); \
-  printf(__VA_ARGS__);printf("\n"); \
+  printf(__VA_ARGS__); \
+}
+#define NDN_LOG_DEBUG_NAME(name) { \
+  ndn_name_print(name);\
 }
 #else
 #define NDN_LOG_DEBUG(...)
+#define NDN_LOG_DEBUG_NAME(name)
 #endif
 
 #ifdef ENABLE_NDN_LOG_INFO
@@ -39,11 +48,15 @@ extern "C" {
   printf("INFO: %s: ", __func__); \
   printf(__VA_ARGS__);printf("\n"); \
 }
+#define NDN_LOG_LOG_NAME(name) { \
+  ndn_name_print(name);\
+}
 #define NDN_LOG_INFO_NO_NEWLINE(...) { \
   printf(__VA_ARGS__); \
 }
 #else
 #define NDN_LOG_INFO(...)
+#define NDN_LOG_INFO_NAME(name)
 #define NDN_LOG_INFO_NO_NEWLINE(...)
 #endif
 
