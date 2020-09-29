a67cfae4b356ab39e59cc4dd03fcaa5f23da4706
Arman Pairavi <arman.pairavi@fokus.fraunhofer.de>
2020-09-29 15:53:04 +0200

    add global forwarder access

--- forwarder/forwarder.c
+++ forwarder/forwarder.c
@@ -11,49 +11,13 @@
 #define ENABLE_NDN_LOG_DEBUG 1
 #define ENABLE_NDN_LOG_ERROR 1
 #include "forwarder.h"
-#include "pit.h"
-#include "fib.h"
-#include "face-table.h"
 #include "../ndn-constants.h"
 #include "../ndn-error-code.h"
 #include "../encode/tlv.h"
 #include "../encode/name.h"
 #include "../util/logger.h"
 
-#define NDN_FORWARDER_RESERVE_SIZE(nametree_size, facetab_size, fib_size, pit_size) \
-  (NDN_NAMETREE_RESERVE_SIZE(nametree_size) + \
-   NDN_FACE_TABLE_RESERVE_SIZE(facetab_size) + \
-   NDN_FIB_RESERVE_SIZE(fib_size) + \
-   NDN_PIT_RESERVE_SIZE(pit_size))
-
-#define NDN_FORWARDER_DEFAULT_SIZE \
-  NDN_FORWARDER_RESERVE_SIZE(NDN_NAMETREE_MAX_SIZE, \
-                             NDN_FACE_TABLE_MAX_SIZE, \
-                             NDN_FIB_MAX_SIZE, \
-                             NDN_PIT_MAX_SIZE)
-
 uint8_t encoding_buf[2048];
-
-/**
- * NDN-Lite forwarder.
- * We will support content store in future versions.
- * The NDN forwarder is a singleton in an application.
- */
-typedef struct ndn_forwarder {
-  ndn_nametree_t* nametree;
-  ndn_face_table_t* facetab;
-
-  /**
-   * The forwarding information base (FIB).
-   */
-  ndn_fib_t* fib;
-  /**
-   * The pending Interest table (PIT).
-   */
-  ndn_pit_t* pit;
-
-  uint8_t memory[NDN_FORWARDER_DEFAULT_SIZE];
-} ndn_forwarder_t;
 
 static ndn_forwarder_t forwarder;
 
@@ -110,6 +74,11 @@ ndn_forwarder_init(void)
   ndn_pit_init(ptr, NDN_PIT_MAX_SIZE, forwarder.nametree);
   forwarder.pit = (ndn_pit_t*)ptr;
   ptr += NDN_PIT_RESERVE_SIZE(NDN_PIT_MAX_SIZE);
+}
+
+const ndn_forwarder_t*
+ndn_forwarder_get(void){
+  return &forwarder;
 }
 
 void
--- forwarder/forwarder.h
+++ forwarder/forwarder.h
@@ -12,16 +12,53 @@
 #define FORWARDER_FORWARDER_H
 
 #include "face.h"
+#include "name-tree.h"
+#include "pit.h"
+#include "fib.h"
+#include "face-table.h"
 #include "../encode/name.h"
 #include "../encode/interest.h"
 #include "callback-funcs.h"
 #include "../util/msg-queue.h"
+
+#define NDN_FORWARDER_RESERVE_SIZE(nametree_size, facetab_size, fib_size, pit_size) \
+  (NDN_NAMETREE_RESERVE_SIZE(nametree_size) + \
+   NDN_FACE_TABLE_RESERVE_SIZE(facetab_size) + \
+   NDN_FIB_RESERVE_SIZE(fib_size) + \
+   NDN_PIT_RESERVE_SIZE(pit_size))
+
+#define NDN_FORWARDER_DEFAULT_SIZE \
+  NDN_FORWARDER_RESERVE_SIZE(NDN_NAMETREE_MAX_SIZE, \
+                             NDN_FACE_TABLE_MAX_SIZE, \
+                             NDN_FIB_MAX_SIZE, \
+                             NDN_PIT_MAX_SIZE)
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 // TODO: Add support of content store and make it a modular component: can be realized with RAM, ROM, etc. the size is configurable.
+
+/**
+ * NDN-Lite forwarder.
+ * We will support content store in future versions.
+ * The NDN forwarder is a singleton in an application.
+ */
+typedef struct ndn_forwarder {
+  ndn_nametree_t* nametree;
+  ndn_face_table_t* facetab;
+
+  /**
+   * The forwarding information base (FIB).
+   */
+  ndn_fib_t* fib;
+  /**
+   * The pending Interest table (PIT).
+   */
+  ndn_pit_t* pit;
+
+  uint8_t memory[NDN_FORWARDER_DEFAULT_SIZE];
+} ndn_forwarder_t;
 
 /**@defgroup NDNFwd Forwarder
  * @brief A lite forwarder.
@@ -37,6 +74,11 @@ extern "C" {
  */
 void
 ndn_forwarder_init(void);
+
+/** Returns the forwarder as a pointer
+ */
+const ndn_forwarder_t*
+ndn_forwarder_get(void);
 
 /** Process event messages.
  *
