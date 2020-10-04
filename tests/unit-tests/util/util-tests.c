c649b0a86e271c24ed4252553a6c616cc72d6122
tianyuan129 <tianyuan@cs.ucla.edu>
2020-10-03 23:02:59 -0700

    fix warnings and modify compliation flags

--- tests/unit-tests/util/util-tests.c
+++ tests/unit-tests/util/util-tests.c
@@ -210,39 +210,39 @@ bool _run_nametree_test(){
   // Functional Test
   // Insert
   uint8_t name1[] = "\x07\x17\x08\x03ndn\x08\x09name-tree\x08\x05test1";
-  nametree_entry_t *ptr1 = ndn_nametree_find_or_insert(nametree, name1, strlen(name1));
+  nametree_entry_t *ptr1 = ndn_nametree_find_or_insert(nametree, name1, strlen((char*)name1));
   CU_ASSERT_PTR_NOT_NULL(ptr1);
 
   uint8_t name2[] = "\x07\x17\x08\x03ndn\x08\x09name-tree\x08\x05test2";
-  nametree_entry_t *ptr2 = ndn_nametree_find_or_insert(nametree, name2, strlen(name2));
+  nametree_entry_t *ptr2 = ndn_nametree_find_or_insert(nametree, name2, strlen((char*)name2));
   CU_ASSERT_PTR_NOT_NULL(ptr2);
 
   uint8_t name3[] = "\x07\x17\x08\x03ndn\x08\x09name-tree\x08\x05test3";
-  nametree_entry_t *ptr3 = ndn_nametree_find_or_insert(nametree, name3, strlen(name3));
+  nametree_entry_t *ptr3 = ndn_nametree_find_or_insert(nametree, name3, strlen((char*)name3));
   CU_ASSERT_PTR_NOT_NULL(ptr3);
 
-  nametree_entry_t *ptr4 = ndn_nametree_find_or_insert(nametree, name2, strlen(name2));
+  nametree_entry_t *ptr4 = ndn_nametree_find_or_insert(nametree, name2, strlen((char*)name2));
   CU_ASSERT_PTR_EQUAL(ptr4, ptr2);
 
   uint8_t name5[] = "\x07\x10\x08\x03ndn\x08\x09name-tree";
-  nametree_entry_t *ptr5 = ndn_nametree_find_or_insert(nametree, name5, strlen(name5));
+  nametree_entry_t *ptr5 = ndn_nametree_find_or_insert(nametree, name5, strlen((char*)name5));
   CU_ASSERT_PTR_NOT_NULL(ptr5);
   CU_ASSERT_PTR_NOT_EQUAL(ptr5->left_child, NDN_INVALID_ID);
 
   // Match
-  nametree_entry_t *ptr6 = ndn_nametree_prefix_match(nametree, name2, strlen(name2), NDN_NAMETREE_FIB_TYPE);
+  nametree_entry_t *ptr6 = ndn_nametree_prefix_match(nametree, name2, strlen((char*)name2), NDN_NAMETREE_FIB_TYPE);
   CU_ASSERT_PTR_NULL(ptr6);
 
   ptr5->fib_id = 1;
-  ptr6 = ndn_nametree_prefix_match(nametree, name2, strlen(name2), NDN_NAMETREE_FIB_TYPE);
+  ptr6 = ndn_nametree_prefix_match(nametree, name2, strlen((char*)name2), NDN_NAMETREE_FIB_TYPE);
   CU_ASSERT_PTR_EQUAL(ptr6, ptr5);
 
-  ptr6 = ndn_nametree_prefix_match(nametree, name2, strlen(name2), NDN_NAMETREE_PIT_TYPE);
+  ptr6 = ndn_nametree_prefix_match(nametree, name2, strlen((char*)name2), NDN_NAMETREE_PIT_TYPE);
   CU_ASSERT_PTR_NULL(ptr6);
 
   ptr5->pit_id = 1;
   ptr2->pit_id = 2;
-  ptr6 = ndn_nametree_prefix_match(nametree, name2, strlen(name2), NDN_NAMETREE_PIT_TYPE);
+  ptr6 = ndn_nametree_prefix_match(nametree, name2, strlen((char*)name2), NDN_NAMETREE_PIT_TYPE);
   CU_ASSERT_PTR_EQUAL(ptr6, ptr2);
 
   // Autoclear test
@@ -251,23 +251,23 @@ bool _run_nametree_test(){
   ndn_nametree_init(nametree, 10);
   for(i = 0; i < 10 - 4; i ++){
     name20[4] = i;
-    ptr1 = ndn_nametree_find_or_insert(nametree, name20, strlen(name20));
+    ptr1 = ndn_nametree_find_or_insert(nametree, name20, strlen((char*)name20));
     ptr1->fib_id = 0;
   }
   uint8_t name21[] = "\x07\x10\x08\x03ndn\x08\x09name-tree";
-  ptr1 = ndn_nametree_find_or_insert(nametree, name21, strlen(name21));
+  ptr1 = ndn_nametree_find_or_insert(nametree, name21, strlen((char*)name21));
   CU_ASSERT_PTR_NOT_NULL(ptr1);
   ptr1->fib_id = 1;
 
   uint8_t name22[] = "\x07\x10\x08\x03nbn\x08\x09name-tree";
-  ptr1 = ndn_nametree_find_or_insert(nametree, name22, strlen(name22));
+  ptr1 = ndn_nametree_find_or_insert(nametree, name22, strlen((char*)name22));
   CU_ASSERT_PTR_NULL(ptr1);
 
-  ptr1 = ndn_nametree_find_or_insert(nametree, name21, strlen(name21));
+  ptr1 = ndn_nametree_find_or_insert(nametree, name21, strlen((char*)name21));
   CU_ASSERT_PTR_NOT_NULL(ptr1);
   ptr1->fib_id = NDN_INVALID_ID;
 
-  ptr1 = ndn_nametree_find_or_insert(nametree, name22, strlen(name22));
+  ptr1 = ndn_nametree_find_or_insert(nametree, name22, strlen((char*)name22));
   CU_ASSERT_PTR_NOT_NULL(ptr1);
 
   return true;
