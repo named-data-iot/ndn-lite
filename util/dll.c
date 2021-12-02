d4dd4d45482042366ed91a5bc4bc79848587f0d7
Arman Pairavi <arman-pairavi@hotmail.de>
2021-12-02 16:39:24 +0100

    add more comments

--- util/dll.c
+++ util/dll.c
@@ -30,9 +30,11 @@ void dll_insert(ndn_cs_entry_t* entry){
 
   dll_entry_t* new_entry = malloc(sizeof(dll_entry_t));
 
+  // create first entry in dll
   if (head == NULL){
     NDN_LOG_DEBUG("The linked list is currently empty, inserting first entry\n");
 
+    // set next and prev pointer to first entry
     head = new_entry;
     head->next = head;
     head->prev = head;
@@ -46,10 +48,13 @@ void dll_insert(ndn_cs_entry_t* entry){
 
   dll_entry_t* last = head->prev;
 
+  // create entry in a not-empty dll
+  // set next and prev pointer of new_entry
   new_entry->next = head;
   new_entry->prev = last;
   new_entry->cs_entry = entry;
 
+  // update pointer of first and last entry
   head->prev = new_entry;
   last->next = new_entry;
 
@@ -68,6 +73,7 @@ void dll_remove_first(void){
   if (head->next == head){
     NDN_LOG_DEBUG("only one entry in dll, removing that\n");
 
+    // free all malloc'd memory and remove CS entry from nametree
     free(head->cs_entry->content);
 
     const ndn_forwarder_t* forwarder = ndn_forwarder_get();
@@ -86,11 +92,15 @@ void dll_remove_first(void){
   dll_entry_t* tmp = head;
   dll_entry_t* last = head->prev;
 
+  // remove first entry in a not-empty dll
+  // update pointer of first and last entry
   head = head->next;
   head->prev = last;
   last->next = head;
 
   NDN_LOG_DEBUG("head pointer after shifting: %p next: %p prev: %p\n", (void*) head, (void*) head->next, (void*) head->prev);
+
+  // free all malloc'd memory and remove CS entry from nametree
   free(tmp->cs_entry->content);
 
   const ndn_forwarder_t* forwarder = ndn_forwarder_get();
@@ -116,6 +126,7 @@ void dll_remove_cs_entry(ndn_cs_entry_t* entry){
     return;
   }
 
+  // check if only one entry in dll and try removing it
   if (head->next == head){
     if (head->cs_entry == entry)
       dll_remove_first();
@@ -129,13 +140,16 @@ void dll_remove_cs_entry(ndn_cs_entry_t* entry){
   dll_entry_t* next = tmp->next;
   dll_entry_t* last = head->prev;
 
+  // step through dll starting at second entry and check for CS entry
   while (tmp != head){
     if (tmp->cs_entry == entry){
       NDN_LOG_DEBUG("CS entry found %p\n", tmp);
 
+      // update pointer of neighbors
       tmp->next->prev = tmp->prev;
       tmp->prev->next = tmp->next;
 
+      // free all malloc'd memory and remove CS entry from nametree
       free(tmp->cs_entry->content);
       ndn_cs_remove_entry(forwarder->cs, tmp->cs_entry);
 
@@ -152,13 +166,16 @@ void dll_remove_cs_entry(ndn_cs_entry_t* entry){
     next = tmp->next;
   }
 
+  // after stepping through dll only head remains
   if (head->cs_entry == entry){
     NDN_LOG_DEBUG("CS entry found at head %p\n", head);
 
+    // update pointer of neighbors
     head = head->next;
     head->prev = tmp->prev;
     last->next = head;
 
+    // free all malloc'd memory and remove CS entry from nametree
     free(tmp->cs_entry->content);
     ndn_cs_remove_entry(forwarder->cs, tmp->cs_entry);
 
@@ -186,7 +203,9 @@ int dll_check_all_cs_entry_freshness(void){
   dll_entry_t* tmp = head->next;
   dll_entry_t* next = tmp->next;
 
+  // step through dll starting at second entry
   while (tmp != head){
+    // check current entry for freshness and remove if not fresh
     if (dll_check_one_cs_entry_freshness(tmp->cs_entry) == -1){
       NDN_LOG_DEBUG("expired entry in CS detected\n");
       counter++;
@@ -197,6 +216,7 @@ int dll_check_all_cs_entry_freshness(void){
     next = tmp->next;
   }
 
+  // only head remains to check for freshness, remove if not fresh
   if (dll_check_one_cs_entry_freshness(head->cs_entry) == -1){
     NDN_LOG_DEBUG("expired entry in CS head detected\n");
     counter++;
@@ -216,6 +236,7 @@ int dll_check_one_cs_entry_freshness(ndn_cs_entry_t* entry){
     return NDN_INVALID_POINTER;
   }
 
+  // check this entry for freshness
   if (entry->fresh_until <= now){
     NDN_LOG_DEBUG("CS entry is not fresh\n");
     return -1;
